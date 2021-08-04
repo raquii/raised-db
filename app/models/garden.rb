7948fbb7e749b73a48fc2a52694765a97a7bf96d
raquel <raquelanaroman@gmail.com>
2021-08-04 16:56:07 -0400

    remove hardiness zone validation

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,13 +1,11 @@
 class Garden < ActiveRecord::Base
     validates :name, presence: true, length: {in: 3..100}
-    validates :length, presence: true, inclusion: { in: 5..100,
+    validates :length, presence: true, inclusion: { in: 1..15,
         message: "%{value} is not a valid length" }
-    validates :width, presence: true, inclusion: { in: 5..100,
+    validates :width, presence: true, inclusion: { in: 1..15,
         message: "%{value} is not a valid width" }
     validates :depth, presence: true, inclusion: { in: 5..30,
         message: "%{value} is not a valid depth" }
-    validates :hardiness_zone, length: { in: 1..3,
-        message: "%{value} is not a valid zone" }
 
     has_many :plants, through: :plots
     has_many :plots
